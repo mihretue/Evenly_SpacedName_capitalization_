e6fbf903947b02efbda064f04444c62974e81b1e
Mihretu Endeshaw <106491338+mihretue@users.noreply.github.com>
2024-07-19 12:07:32 +0300

    done also for none even spaced names

--- for_none_even_spaced_names.py
+++ for_none_even_spaced_names.py
@@ -3,3 +3,20 @@ import os
 import random
 import re
 import math
+
+
+def solve(s):
+    # Write your code here
+    capital_names = [char.upper() if (i == 0 or s[i-1]==' ') else char for i, char in enumerate(s)]
+    
+    fullname_after_capitalization = ''.join(capital_names)
+    
+    return fullname_after_capitalization
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    s = input()
+    result = solve(s)
+    fptr.write(result + '\n')
+    
+    fptr.close()
